5edfb406ae20a6bc9998ba65986a7d3e0ef8e7e8
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-01 17:35:47 +0400

    Add files via upload

---
+++ P336_cs_array/P336_cs_array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static int[] CreateArray(int length, int value)
+    {
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = value;
+        }
+
+        return array;
+    }
+    static void Main()
+    {
+        int length = 7;
+        int value = 14;
+
+        int[] newArray = CreateArray(length, value);
+
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
